7b0ad22f61d75583874769b7b26b2b070b162068
Sakis Karagiannis <sakiskar90@gmail.com>
2023-04-15 15:15:23 +0100

    CHG: Bind random port for integration tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,13 @@
+use std::fmt::format;
+use std::net::TcpListener;
+
 #[actix_web::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
-    let response = client.get("http://127.0.0.1:8080/health_check")
+    let response = client.get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,8 +16,13 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to start server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to start server");
 
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
