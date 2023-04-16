7e7a1eed1637a65d5449af3c81996d72d8bc49c2
Sakis Karagiannis <sakiskar90@gmail.com>
2023-04-16 10:35:21 +0100

    ADD: Database logic

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,37 @@
 use std::net::TcpListener;
 
-#[actix_web::test]
-async fn health_check_works() {
-    let address = spawn_app();
+use sqlx::PgPool;
+
+use zero2prod::startup::run;
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app(connection_pool: PgPool) -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+#[sqlx::test]
+async fn health_check_works(pool: PgPool) {
+    let app = spawn_app(pool).await;
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,25 +40,16 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
+#[sqlx::test]
+async fn subscribe_returns_a_200_for_valid_form_data(pool: PgPool) -> () {
+    let app = spawn_app(pool).await;
 
-    let server = zero2prod::startup::run(listener).expect("Failed to start server");
-
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
-}
-
-#[actix_web::test]
-async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app_address = spawn_app();
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -42,11 +57,19 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
-#[actix_web::test]
-async fn subscribe_returns_a_400_when_data_is_missing() {
-    let app_address = spawn_app();
+#[sqlx::test]
+async fn subscribe_returns_a_400_when_data_is_missing(pool: PgPool) {
+    let app = spawn_app(pool).await;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
@@ -57,7 +80,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(format!("{}/subscriptions", &app_address))
+            .post(format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
