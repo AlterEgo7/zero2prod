7c643f7befed9d6c422437f9b67874b0c227645f
Sakis Karagiannis <sakiskar90@gmail.com>
2023-04-20 18:09:55 +0100

    ADD: jaeger tracing

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,3 +1,5 @@
+use opentelemetry::global;
+use opentelemetry::sdk::propagation::TraceContextPropagator;
 use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
@@ -5,17 +7,30 @@ use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-pub fn get_subscriber<Sink>(name: String, env_filter: String, sink: Sink) -> impl Subscriber + Send + Sync
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
 where
     Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
 {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name("zero2prod")
+        .install_batch(opentelemetry::runtime::TokioCurrentThread)
+        .expect("Failed to install OpenTelemetry tracer");
+
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
 
     let formatting_layer = BunyanFormattingLayer::new(name, sink);
 
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     Registry::default()
         .with(env_filter)
+        .with(telemetry)
         .with(JsonStorageLayer)
         .with(formatting_layer)
 }
