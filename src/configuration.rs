1b2a22067b5bc8d36d4423905699be3c5c35d4ab
Sakis Karagiannis <sakiskar90@gmail.com>
2023-04-23 10:08:03 +0100

    ADD: Environments for config management

--- src/configuration.rs
+++ src/configuration.rs
@@ -19,10 +19,55 @@ pub struct DatabaseSettings {
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
     pub port: u16,
-    pub host: String
+    pub host: String,
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    let default_builder = Config::builder()
+        .add_source(File::from(configuration_directory.join("base")).required(true))
+        .add_source(File::from(configuration_directory.join(environment.as_str())).required(true));
+
+    default_builder.build()?.try_deserialize()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
+
+pub fn get_configuration_old() -> Result<Settings, config::ConfigError> {
     let builder = Config::builder().add_source(File::with_name("configuration"));
 
     builder.build()?.try_deserialize()
