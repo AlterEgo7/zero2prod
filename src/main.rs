28abd1047a74e4d00799d0b85e66cfc517606e76
Sakis Karagiannis <sakiskar90@gmail.com>
2023-09-25 17:22:33 +0100

    CHG: Refactor application setup code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
-
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::*;
 
 #[actix_web::main]
@@ -14,26 +9,7 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorisation_token,
-        timeout,
-    );
-
-    let address = format!("127.0.0.1:{}", configuration.application.port);
-
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+    let application = Application::build(configuration, None).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
